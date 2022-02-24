49bfff3ee581a7ad3e0bd6eac340451ce00cefb8
Marcolux <mccolucci1@gmail.com>
2022-02-24 09:40:35 -0600

    added the about me section

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let displayMenuLeft = document.getElementById("displayMenuLeft")
 let displayMenuRight = document.getElementById("displayMenuRight")
 let navigationBar = document.getElementById("navigationBar")
 
+let aboutMe = document.getElementById('aboutMe')
+let aboutMeLink = document.getElementById('aboutMeLink')
+
+
 let projects = document.getElementById('Projects')
 let nav = document.querySelector('nav')
 let projectsLink = document.getElementById('projectsLink')
@@ -14,21 +18,24 @@ let footer= document.querySelector('footer')
 displayMenuLeft.addEventListener('click',()=>{
     console.log('hi')
     navigationBar.style.width="80vw"
-    displayMenuRight.style.right='99%'
-    displayMenuLeft.style.right='99%'
+    displayMenuRight.style.right='100%'
+    displayMenuLeft.style.right='100%'
     navigationBar.style.fontSize="3vh"
     projects.style.height='0vh'
     projects.style.display='flex'
     displayMenuRight.style.transition = '.25s ease'
     displayMenuLeft.style.transition = '.25s ease'
     footer.style.height='0vh'
+    aboutMe.style.height='0vh'
+    aboutMe.style.display='flex'
     
 
 
-    if(displayMenuRight.style.right='99%'){
+    if(displayMenuRight.style.right='100%'){
         displayMenuLeft.style.display='none'
         displayMenuRight.style.display='flex'
         nav.style.width = '98vw'
+      
     }
 })
 
@@ -42,6 +49,7 @@ displayMenuRight.addEventListener('click',()=>{
     displayMenuRight.style.transition = '1s ease'
     projects.style.display = 'none';
     footer.style.height='0vh'
+    aboutMe.style.display='none'
 
     if(displayMenuRight.style.right==='1px')
     {   displayMenuRight.style.display='none'
@@ -52,13 +60,22 @@ displayMenuRight.addEventListener('click',()=>{
 projectsLink.addEventListener('click', ()=>{
     console.log('hi')
     if(projects.style.height === '0vh'){
-        // projects.style.display = 'flex'
+        aboutMe.style.height= '0vh'
         projects.style.height= '90vh';}
     else{
         projects.style.height= '0vh'
         
     }
     
+})
+
+aboutMeLink.addEventListener('click',()=>{
+    if(aboutMe.style.height === '0vh'){
+        aboutMe.style.height= '90vh'
+        projects.style.height = '0vh'
+    }else{
+        aboutMe.style.height= '0vh'
+    }
 })
 
 contactsLink.addEventListener('click',()=>{
@@ -68,3 +85,8 @@ footer.style.height='10vh'
 }else if(footer.style.height==='10vh'){
     footer.style.height='0vh'}
 })
+
+
+class SoftwareEngineer{
+    constructor(fullstack){}
+}
